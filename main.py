00404233d60ea98d261586150975851f0afcd228
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2020-02-09 15:28:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import random
 import math
-import 
+import turtle
+import sqlite3
+def blank():
+    pass
+
+class Command:
+    self.query = ""
+    self.fn    = blank()
+    self.commType = "action"
+    self.name     = "blank_script"
+    def __init__(q,f,n):
+        self.query = q
+        self.fn    = f
+        self.name  = n
+    
+    def config(**configs):
+        for item in configs.keys():
+            if item in ["query","function","name","commType"]:
+                
+
+
