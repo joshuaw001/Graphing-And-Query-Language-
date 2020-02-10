3a53fe513c4a57c4d576cfd2fbf13f08b231a7a0
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2020-02-09 19:26:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class Command:
     self.fn    = blank()
     self.commType = "action"
     self.name     = "blank_script"
+
     def __init__(q,f,n):
         self.query = q
         self.fn    = f
@@ -31,3 +32,13 @@ class Command:
                         return "FAIL"
                 else:
                     return "FAIL"
+    def getType():
+        w = [Action:"RUN {self.query}({self.args});",Getter:{["GET {self.query}({self.args});"],"DEFINE VAR(~,~) TO VALUE({self.query}({self.args});"],Setter:["{self.query}({self.args})||SETTER;"]}
+        if self.query == w["Action"][0]:
+            return "TYPEOF({}) = 'Action';".format(self.name)
+        elif self.query == w["Getter"][0] OR self.query == w["Getter"][1]:
+            return "TYPEOF({}) = 'Getter';".format(self.name)
+        elif self.query == w["Setter"][0]:
+            return "TYPEOF({}) = 'Setter';".format(self.name)
+        else:
+            return "FAIL"
